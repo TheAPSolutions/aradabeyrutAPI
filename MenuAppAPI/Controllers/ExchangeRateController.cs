fcd286bb51b8a20384c13412b4ad4ed1eeb3e9e5
M7mmedHomsi <m7mmedhomsi@gmail.com>
2024-12-15 14:06:47 +0300

    exchnage rate

---
+++ MenuAppAPI/Controllers/ExchangeRateController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AradaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExchangeRateController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public ExchangeRateController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpGet("usd-try")]
+        public async Task<IActionResult> GetUsdToTryRate()
+        {
+            var url = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            try
+            {
+                var response = await _httpClient.GetStringAsync(url);
+                return Content(response, "application/xml");
+            }
+            catch
+            {
+                return StatusCode(500, "Error fetching exchange rates from TCMB.");
+            }
+        }
+    }
+}
